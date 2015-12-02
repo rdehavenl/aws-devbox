6a689d9c2d28c17a90c893a82caf22a02df837c3
Richard DeHaven <thatguy@rldmail.com>
2015-12-01 19:46:03 -0500

    Updated script to make it nicer to watch
      - Added a check_ssh function that output's '.' while waiting
      - Reduced the sleep timeout before checking for hostname

--- boot-instance.sh
+++ boot-instance.sh
@@ -6,6 +6,18 @@
 #
 # By Richard DeHaven (2015/11/08)
 
+check_ssh () {
+  ssh_check=1
+  ssh_check_count=0
+  echo "[BOOTING]"
+  while [ "${ssh_check}" != 0 ]; do
+  nc -z -w5 ${instance_hostname} 22 >/dev/null
+  ssh_check=$?
+  echo -n '.'
+  done
+  echo "DONE!"
+}
+
 ### Boot the instance
 echo "Booting instance"
 echo "[INFO] Boot String: ec2-run-instances ami-61bbf104 -t t2.micro -k 'rdehaven@rldemail.com' -g 'default'"
@@ -13,15 +25,15 @@ instance_id=$(ec2-run-instances ami-61bbf104 -t t2.micro -k 'rdehaven@rldemail.c
 
 ### Give it some time to associate the public IP
 echo "Waiting for Boot"
-sleep 10
+sleep 5
 
 ### Get the hostname of the new instance
 echo "Fetching Hostname"
-echo "[INFO] Command: ec2-describe-network-interfaces -F "attachment.instance-id=${instance_id}" | grep ASSOCIATION | awk '{ print \$5}'"
 instance_hostname=$(ec2-describe-network-interfaces -F "attachment.instance-id=${instance_id}" | grep ASSOCIATION | awk '{ print $5}')
 
-echo "Setting Vars in Stat File"
+check_ssh
+
+echo "Setting Params in Stat File"
+echo "[INFO] AWS_HOSTNAME: ${instance_hostname}"
 sed -i "s/AWS_INSTANCE_ID=.*/AWS_INSTANCE_ID=${instance_id}/" $HOME/.aws_my_instance 
 sed -i "s/AWS_INSTANCE_HOSTNAME=.*/AWS_INSTANCE_HOSTNAME=${instance_hostname}/" $HOME/.aws_my_instance
-
-source $HOME/.aws_my_instance
